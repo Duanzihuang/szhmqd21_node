23a0482bbe486c81da6645690360d8ac83842911
duanzihuang <huangwei@itcast.cn>
2018-08-30 13:06:44 +0800

    11-完成pm2配置文件的创建及修改

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name      : 'szhmqd21_node',
+    script    : './src/app.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '39.108.131.40',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Duanzihuang/szhmqd21_node.git',
+      path : '/ftp/node/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
